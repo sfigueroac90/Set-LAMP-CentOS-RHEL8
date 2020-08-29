e06719e9bd9627551c37bcc9317092245df8dc19
Sebastián Figueroa <sebastian.figueroa@parqueexplora.org>
2020-08-28 19:05:28 -0500

    set-user.sh

--- set-user.sh
+++ set-user.sh
@@ -5,7 +5,7 @@ read -sp 'Paswoord: ' password
 
 #export USER_NAME=worpdressadmin
 sudo adduser $Username 
-echo $($password) | passwd --stdin
+echo $password | sudo passwd -u $Username -f --stdin 
 sudo gpasswd -a $Username wheel
 
 #Copy Recursively the files from original user to new user
